287f1ebe92533f3cf54ec3d3ea85f53c7c6a3253
MuhammadUzair <113247547+lilraja-x@users.noreply.github.com>
2024-08-18 17:23:14 +0500

    fixed imports

--- commons/decorator.py
+++ commons/decorator.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import request,jsonify
-from commons.utils import HttpCodes
+from .utils import HttpCodes
 from users.models import Users
 
 def verify_token(token):
